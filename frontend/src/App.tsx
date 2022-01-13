614cdc1f498e8560b260ca8253eb39e3ead7f132
João Paulo Lima <jptick@gmail.com>
2022-01-13 11:12:11 -0300

    Create Components Movie and Pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import NavBar from 'components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from 'components/Navbar';
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
 
 function App() {
-  return <NavBar />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
